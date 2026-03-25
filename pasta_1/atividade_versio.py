68b9832910d547d6fadb7944d3f4955295a0b00c
Fabian De Jesus Romero Corona <00001242465200sp@aluno.educacao.sp.gov.br>
2026-03-25 15:45:22 -0300

    Entrega de fabian (cubano)

--- pasta_1/atividade_versio.py
+++ pasta_1/atividade_versio.py
@@ -2,10 +2,10 @@
 # Print ('Consegui, Prof! Assinado: Nome do aluno')
 #Insira o commit abaixo do comando
 
-nome = input('Insira seu nome: ')
+nome = input('Insira seu nome:Fabian (cubano)')
 print(f'Parabéns {nome}! Nota 10!')
 
 print('Professor Caio Esteve Aqui! Também corrigi um erro')
 print('Professor Caio Passou Novamente Aqui') #Inseri um novo comentario
 print('Agora vai')
-print('eu estive aqui (fabian)')
+print('eu estive aqui Cubano (fabian)')
